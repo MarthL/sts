7f792dc4595ab3355dc30df08a40734235159cb4
martin <martin.aubertin@90tech.fr>
2023-08-17 10:44:01 +0200

    auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavBar } from './components/organisms/NavBar/NavBar';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Container } from '@mui/material';
 import { ProfilePage } from './components/templates/ProfilePage/ProfilePage';
 import { HomePage } from './components/templates/HomePage/HomePage';
+import { LoginPage } from './components/templates/LoginPage/LoginPage';
 
 
 function App() {
+  const [isConnected, setIsConnected] = useState(false); // for login
+
   return (
     <>
-      <NavBar />
-      <Container maxWidth="xl" sx={{ marginTop: "10px" }}>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/profile" element={<ProfilePage />}></Route>
-        </Routes>
-      </Container>
+      <Router>
+        <NavBar />
+        {isConnected ? (
+          <>
+            <Container maxWidth="xl" sx={{ marginTop: "10px" }}>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+              </Routes>
+            </Container>
+          </>
+        ) : (
+          <LoginPage />
+        )}
+      </Router>
     </>
   );
 }
