6c978ddd2f727b6db68c2d24c1382138b1add42f
martin <martin.aubertin@90tech.fr>
2023-08-19 16:50:02 +0200

    FE add

--- frontend/src/components/templates/HomePage/HomePage.tsx
+++ frontend/src/components/templates/HomePage/HomePage.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { CardProject } from '../../molecules/CardProject/CardProject';
 import { Box, Typography } from '@mui/material';
 import { getProjects } from '../../../api/projects';
+import { getCurrentUser } from '../../../api/users';
 
 export const HomePage = () => {
 
   const [projectsCollection, setProjectsCollection] = useState([]);
+  const [currentUser, setCurrentUser] = useState('');
   const [isLoaded, setIsLoaded] = useState(false);
 
 
@@ -16,10 +18,16 @@ export const HomePage = () => {
     });
   }, [])
 
+  useEffect(() => {
+    getCurrentUser().then(async (res) => {
+      setCurrentUser(res.username);
+    });
+  }, [])
+
 
   return (
     <>
-      <Typography variant="h3" color="initial"> Welcome, John Doe </Typography>
+      <Typography variant="h3" color="initial"> Welcome, {currentUser[0].toUpperCase() + currentUser.slice(1)} </Typography>
       <Box display={'flex'} flexDirection={'row'} justifyContent={'center'} flexWrap={'wrap'} gap={20}>
         {
           projectsCollection.map((project: any) => {
