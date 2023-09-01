3e8fc53877ded52c3181ecf22555ff2fc5c068a1
martin <martin.aubertin@90tech.fr>
2023-09-02 00:41:55 +0200

    add collaborators and request is working correclty

--- frontend/src/components/templates/CollaboratorsPage/CollaboratorsPage.tsx
+++ frontend/src/components/templates/CollaboratorsPage/CollaboratorsPage.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAllUsers } from '../../../api/users';
 
 export const CollaboratorsPage = () => {
+
+
+  const [userCollection, setUserCollection] = useState(null);
+
+  useEffect(() => {
+    getAllUsers().then(async (res) => {
+      setUserCollection(res);
+    })
+  }, [])
+
+  console.log(userCollection);
+
   return (
     <></>
   )
