57fa2d7ced1deca27e1178c0e1cbbe754bd43c92
martin <martin.aubertin@90tech.fr>
2024-02-03 05:38:00 +0100

    test implementation for backend

--- backend/src/projects/projects.controller.spec.ts
+++ backend/src/projects/projects.controller.spec.ts
@@ -1,33 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { Projects } from './projects.entity';
 
-describe('ProjectsService', () => {
-  let service: ProjectsService;
+describe('ProjectsController', () => {
+  let projectsController: ProjectsController;
+  let projectsService: ProjectsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      controllers: [ProjectsController],
       providers: [
         ProjectsService,
         {
           provide: getRepositoryToken(Projects),
-          useValue: {},
+          useClass: Repository,
         },
       ],
     }).compile();
 
-    service = module.get<ProjectsService>(ProjectsService);
+    projectsService = module.get<ProjectsService>(ProjectsService);
+    projectsController = module.get<ProjectsController>(ProjectsController);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-    console.log('service -> ', service);
-  });
+  describe('findAll', () => {
+    it('should return an array of projects', async () => {
+      const result: Projects[] = Array.from({ length: 5 }, (_, index) => ({
+        id: index + 1,
+        project_name: `Project ${index + 1}`,
+        description: `Description ${index + 1}`,
+      }));
 
-  it('should get a project by id', async () => {
-    const projectId = 1;
-    const project = await service.getProjectById(projectId);
-    expect(project).toBeDefined();
+      jest.spyOn(projectsService, 'getProjects').mockResolvedValue(result);
+
+      await expect(projectsController.getAll()).resolves.toEqual(result);
+    });
   });
 });
