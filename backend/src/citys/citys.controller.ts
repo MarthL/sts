b8dfed89970038ef33431e6d9339ac730c7fbd90
Vivi55100 <steven.halliez@gmail.com>
2024-02-06 14:11:08 +0100

    Add patch by id method on city

--- backend/src/citys/citys.controller.ts
+++ backend/src/citys/citys.controller.ts
@@ -3,12 +3,15 @@ import {
     Get,
     Param,
     ParseIntPipe,
+    Patch,
+    Body
   } from '@nestjs/common';
   import { ApiTags } from '@nestjs/swagger';
   
   import { CitysService } from './citys.service';
   
   import { Citys } from './citys.entity';
+import CitysResponseDto from 'src/DTO/Citys/citysResponse.dto';
 
   @ApiTags('Citys')
   @Controller('/citys')
@@ -23,6 +26,14 @@ import {
     @Get(':id')
     async getCityById(@Param('id', ParseIntPipe) id: number) {
       return await this.citysService.getCityById(id);
+    }
+
+    @Patch(':id')
+    async updateCities(
+      @Param('id') id: number,
+      @Body() updateReq: CitysResponseDto,
+    ): Promise<CitysResponseDto> {
+      return await this.citysService.patch(id, updateReq);
     }
   }
   
