b8a0ef2e8f9d649b3229ced7e7e8823dc44f201a
Vivi55100 <steven.halliez@gmail.com>
2024-02-15 14:52:19 +0100

    Test company ok

---
+++ backend/src/DTO/Company/companyResponse.dto.spec.ts
@@ -0,0 +1,45 @@
+import { validate } from "class-validator";
+import { randomInt } from "crypto";
+import { CompanyResponseDto } from "./companyResponse.dto";
+
+describe('CompanyResponseDto', () => {
+    it('should pass validation when all properties are present', async () => {
+        const dto = new CompanyResponseDto();
+        dto.id = randomInt(10);
+        dto.name = 'Test name of company';
+        const errors = await validate(dto);
+        expect(errors.length).toBe(0);
+    });
+
+    it('should fail validation when ID is missing', async () => {
+        const dto = new CompanyResponseDto();
+        dto.name = 'Test name of company';
+        const errors = await validate(dto);
+        expect(errors.length).toBeGreaterThanOrEqual(1);
+    });
+
+    it('should fail validation when ID is not a number', async () => {
+        let dto = new CompanyResponseDto();
+        Object.defineProperty(dto, 'id', {
+            get: function() {
+              return this._id + '';
+            },
+            set: function(value) {
+              this._id = value;
+            },
+            enumerable: true,
+            configurable: true
+          });
+        dto.id = 1;
+        dto.name = 'Test name of company';
+        const errors = await validate(dto);
+        expect(errors.length).toBeGreaterThanOrEqual(1);
+    });
+
+    it('should fail validation when type is not same', async () => {
+        const dto = new CompanyResponseDto();
+        dto.name = 'This is a test for name company';
+        const errors = await validate(dto);
+        expect.stringContaining(dto.name);
+    });
+})
---
+++ backend/src/DTO/Company/createCompany.dto.spec.ts
@@ -0,0 +1,17 @@
+import { validate } from "class-validator";
+import { CreateCompanyDto } from "./createCompany.dto";
+
+describe('CreateCompanyDto', () => {
+    it('should fail validation when name is missing', async () => {
+        const dto = new CreateCompanyDto();
+        const errors = await validate(dto);
+        expect(errors.length).toBeGreaterThanOrEqual(1);
+    });
+
+    it('should fail validation when type is not same', async () => {
+        const dto = new CreateCompanyDto();
+        dto.name = 'This is a test for name';
+        const errors = await validate(dto);
+        expect.stringContaining(dto.name);
+    });
+})
