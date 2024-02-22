4c3b451c4a6da1e8f1bb72cd1bc01f727218825d
Vivi55100 <steven.halliez@gmail.com>
2024-02-22 12:32:31 +0100

    Create a button create project

--- frontend/src/components/templates/HomePage/HomePage.tsx
+++ frontend/src/components/templates/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { CardProject } from '../../molecules/CardProject/CardProject';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Button, Modal } from '@mui/material';
 import { ChartDashboard } from '../../organisms/ChartDashboard/ChartDashboard';
 import { getProjects } from '../../../api/projects';
 
@@ -9,6 +9,7 @@ export const HomePage = () => {
   const [projectsCollection, setProjectsCollection] = useState([]);
   const [currentUser, setCurrentUser] = useState('');
   const [isLoaded, setIsLoaded] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
     getProjects().then(async (res) => {
@@ -22,9 +23,37 @@ export const HomePage = () => {
   }, [])
 
 
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+
   return (
     <>
       <Typography variant="h3"> Welcome, {currentUser} </Typography>
+
+      <Button variant="contained" onClick={handleOpenModal}>Créer un projet</Button>
+      
+      <Modal open={openModal} onClose={handleCloseModal}>
+        <Box sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: 600, bgcolor: 'background.paper',
+          border: '2px solid #000',
+          boxShadow: 24,
+          p: 4 }}>
+          <Typography variant="h5" sx={{textAlign:"center"}}>Formulaire de création de projet</Typography>
+          
+          <Button variant="contained" onClick={handleCloseModal}>Fermer</Button>
+        </Box>
+      </Modal>
+      
+
       <ChartDashboard />
       <Box display={'flex'} flexDirection={'row'} justifyContent={'center'} flexWrap={'wrap'} gap={20}>
         {
