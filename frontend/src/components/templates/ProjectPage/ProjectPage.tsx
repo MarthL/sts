ebc829ce3172f8d19497264f4a24f8543a19f27c
martin <martin.aubertin@90tech.fr>
2024-02-27 04:36:33 +0100

    add projectpage, in progress

--- frontend/src/components/templates/ProjectPage/ProjectPage.tsx
+++ frontend/src/components/templates/ProjectPage/ProjectPage.tsx
@@ -2,14 +2,12 @@ import { useState, useEffect } from "react";
 import { getProjectById } from "../../../api/projects";
 import { useParams } from "react-router";
 import { Project } from "../../../api/projects"
-import { useQuery } from "react-query";
 import { Typography, Box, Paper, Container } from "@mui/material"
 
 export const ProjectPage = () => {
 
   const { id } = useParams<{ id: string }>();
   const [project, setProject] = useState<Project>();
-  const [error, setError] = useState(false)
 
   useEffect(() => {
     if (id !== undefined) {
@@ -33,7 +31,9 @@ export const ProjectPage = () => {
       </Box>
       <Box component={"div"}>
         <Paper square={false} elevation={24} sx={{ width: '80vw' }}>
-          <Typography>it works! Project ID: {id}</Typography>
+          <Typography>{project?.id}</Typography>
+          <Typography>{project?.project_name}</Typography>
+          <Typography>{project?.description}</Typography>
         </Paper>
       </Box>
     </>
