33581ef6cec940e0b8e076b8024c2aeeeba26c50
martin <martin.aubertin@90tech.fr>
2024-03-17 20:30:35 +0100

    Design projectpage

--- frontend/src/components/templates/ProjectPage/ProjectPage.tsx
+++ frontend/src/components/templates/ProjectPage/ProjectPage.tsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import { getProjectById } from "../../../api/projects";
 import { useParams } from "react-router";
 import { Project } from "../../../api/projects"
-import { Typography, Box, Paper, Container } from "@mui/material"
+import { Typography, Box, Paper, Container, Grid } from "@mui/material"
+import React from "react";
 
 export const ProjectPage: React.FC<any> = () => {
 
@@ -20,22 +21,23 @@ export const ProjectPage: React.FC<any> = () => {
 
   return (
     <>
-      <Box
-        component={"div"}
-        sx={{
-          borderRadius: '8px',
-          objectFit: "cover"
-        }}
-      >
-        <img src={'https://picsum.photos/800/300'} width={'100%'} alt="hero" />
-      </Box>
       <Box component={"div"}>
-        <Paper square={false} elevation={24} sx={{ width: '80vw' }}>
-          <Typography>{project?.id}</Typography>
-          <Typography>{project?.project_name}</Typography>
-          <Typography>{project?.description}</Typography>
+        <Paper square={true} elevation={24} sx={{ width: '80vw' }}>
+          <Typography variant="h2">Project Name : {project?.project_name}</Typography>
+          {/* <Typography>{project?.id}</Typography> */}
         </Paper>
       </Box>
+      <Grid container mt={5}>
+        <Grid xs={6} item>
+          <Typography variant="body1">{project?.description}</Typography>
+        </Grid>
+        <Grid
+          xs={6}
+          item
+        >
+          <img src={'https://picsum.photos/800/300'} width={'contain'} alt="hero" />
+        </Grid>
+      </Grid>
     </>
   );
 };
