6901abfce074957c27fe6be76d0fded5c8fae580
Vivi55100 <steven.halliez@hotmail.fr>
2024-03-19 13:53:10 +0100

    User min & max characters length

--- backend/src/users/users.entity.ts
+++ backend/src/users/users.entity.ts
@@ -11,6 +11,7 @@ import { Job } from '../job/job.entity';
 import { Projects } from '../projects/projects.entity';
 import { Companys } from '../companys/company.entity';
 import { Citys } from '../citys/citys.entity';
+import { MaxLength } from 'class-validator';
 
 @Entity()
 export class Users {
@@ -18,24 +19,30 @@ export class Users {
   id: number;
 
   @Column('varchar', { length: 50 })
+  @MaxLength(50)
   username: string;
 
   @Column('varchar', { length: 72 })
+  @MaxLength(72)
   password: string;
 
   @Column('varchar', { nullable: true, default: null, length: 100 })
+  @MaxLength(100)
   family_name: string;
 
-  @Column('int', { nullable: true, default: null, width: 2 })
+  @Column('int', { nullable: true, default: null })
   yop: number;
 
   @Column('varchar', { nullable: true, default: '', length: 12 })
+  @MaxLength(12)
   phone_number: string;
 
   @Column('varchar', { nullable: true, default: null, length: 200 })
+  @MaxLength(200)
   email: string;
 
   @Column('varchar', { nullable: true, default: null, length: 255 })
+  @MaxLength(255)
   address: string;
 
   @ManyToOne(() => Job, (job) => job.users)
