2faf22820e30f18ee5befe362a3d4047b6f45db7
Vivi55100 <steven.halliez@hotmail.fr>
2024-03-19 14:01:32 +0100

    All job field characters max length

--- backend/src/job-field/job-field.entity.ts
+++ backend/src/job-field/job-field.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { Job } from '../job/job.entity';
+import { MaxLength } from 'class-validator';
 
 @Entity()
 export class JobField {
@@ -7,6 +8,7 @@ export class JobField {
   id: number;
 
   @Column('varchar', { length: 100 })
+  @MaxLength(100)
   name: string;
 
   @OneToMany(() => Job, (job) => job.jobField)
