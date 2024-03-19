5c562da8f6084b292c1dc8106063950b924d42b5
Vivi55100 <steven.halliez@hotmail.fr>
2024-03-19 14:10:13 +0100

    All city characters max length

--- backend/src/DTO/Citys/citysResponse.dto.ts
+++ backend/src/DTO/Citys/citysResponse.dto.ts
@@ -1,15 +1,18 @@
-import { IsNotEmpty, IsInt } from 'class-validator';
+import { IsNotEmpty, IsInt, MaxLength } from 'class-validator';
 
 export class CitysResponseDto {
   @IsInt()
   id: number;
 
   @IsNotEmpty()
+  @MaxLength(50)
   city_name: string;
 
   @IsNotEmpty()
+  @MaxLength(5)
   zip_code: string;
 
   @IsNotEmpty()
+  @MaxLength(170)
   state: string;
 }
--- backend/src/citys/citys.entity.ts
+++ backend/src/citys/citys.entity.ts
@@ -1,6 +1,7 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 import { OneToMany } from 'typeorm';
 import { Users } from '../users/users.entity';
+import { MaxLength } from 'class-validator';
 
 @Entity()
 export class Citys {
@@ -8,12 +9,15 @@ export class Citys {
   id: number;
 
   @Column('varchar', { length: 50 })
+  @MaxLength(50)
   city_name: string;
 
   @Column('varchar', { length: 5 })
+  @MaxLength(5)
   zip_code: string;
 
   @Column('varchar', { length: 170 })
+  @MaxLength(170)
   state: string;
 
   @OneToMany(() => Users, (user) => user.city)
