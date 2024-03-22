ea781f743329bf22b753982be32cbdb975607029
Vivi55100 <steven.halliez@hotmail.fr>
2024-03-22 17:44:13 +0100

    add multiline option and minRows props

--- frontend/src/components/atoms/TextBlock/TextBlock.tsx
+++ frontend/src/components/atoms/TextBlock/TextBlock.tsx
@@ -4,15 +4,18 @@ import React from 'react';
 interface TextBlockProps extends React.InputHTMLAttributes<HTMLInputElement> {
     value: string;
     variant: TextFieldVariants;
+    minRows: number;
 }
 
 export const TextBlock: React.FC<TextBlockProps> = (props: TextBlockProps) => {
-    const { value, variant } = props;
+    const { value, variant, minRows } = props;
   return (
     <TextField
         type={'text'}
         value={value}
         variant={variant}
+        multiline
+        minRows={minRows}
     />
   );
 };
