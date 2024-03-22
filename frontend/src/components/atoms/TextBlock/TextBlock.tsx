f91c2ac3abfa014ff4212f1852f112e2fe081813
Vivi55100 <steven.halliez@hotmail.fr>
2024-03-22 17:20:32 +0100

    rebase

--- frontend/src/components/atoms/TextBlock/TextBlock.tsx
+++ frontend/src/components/atoms/TextBlock/TextBlock.tsx
@@ -1,26 +1,17 @@
+import { TextField } from '@mui/material';
 import React from 'react';
-import { useFormContext } from 'react-hook-form';
 
 interface TextBlockProps extends React.InputHTMLAttributes<HTMLInputElement> {
     type: string;
     value: string;
-    registerProps: string;
-    onChangeEvent: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const TextBlock: React.FC<TextBlockProps> = (props: TextBlockProps) => {
-    const { type, value, registerProps, onChangeEvent } = props;
-    const register = useFormContext().register;
+    const { type, value } = props;
   return (
-    <>
-        <TextBlock
-            registerProps={registerProps}
-            onChangeEvent={onChangeEvent}
-            //onChange={onChangeEvent}
-            type={type}
-            value={value}
-            {...register(registerProps)}            
-        />
-    </>
+    <TextField
+        type={type}
+        value={value}
+    />
   );
 };
