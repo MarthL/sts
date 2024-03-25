9c1fa14c717586eb8f19ff353e8ac8b03288e346
Vivi55100 <steven.halliez@hotmail.fr>
2024-03-25 14:50:21 +0100

    remove TextBlock and imports in UserPage.tsx file ( it's just a test )

--- frontend/src/components/templates/UserPage/UserPage.tsx
+++ frontend/src/components/templates/UserPage/UserPage.tsx
@@ -1,9 +1,8 @@
-import { Grid, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { User, getUserById } from './../../../api/users';
 import { useNavigate } from 'react-router-dom';
-import { TextBlock } from "../../atoms/TextBlock/TextBlock";
 
 export const UserPage: React.FC<any> = () => {
 
@@ -25,16 +24,6 @@ export const UserPage: React.FC<any> = () => {
     <> 
 
       <Typography> it works !  </Typography>
-      
-      <Grid container>
-        <Grid item xs={12} sx={{mt:2}}>
-          <TextBlock
-            variant="outlined"
-            rows={10}
-            maxChars={500}
-          />
-        </Grid>
-      </Grid>
       
     </>
   )
