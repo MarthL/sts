0aa6e2532075aabe5ac5e23878ca01fcba8b023a
Vivi55100 <steven.halliez@hotmail.fr>
2024-03-28 09:30:44 +0100

    remove last change

--- backend/src/status/status.module.ts
+++ backend/src/status/status.module.ts
@@ -3,20 +3,13 @@ import { StatusService } from './status.service';
 import { StatusController } from './status.controller';
 import { Status } from './status.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { StatusSeederService } from './status-seeder.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Status]),
-    TypeOrmModule.forRootAsync({
-      useFactory: async (statusSeederService: StatusSeederService) => {
-        return await statusSeederService.seedInitialData();
-      },
-      inject: [StatusSeederService],
-    }),
+    TypeOrmModule.forFeature([Status])
   ],
   controllers: [StatusController],
-  providers: [StatusService, StatusSeederService],
+  providers: [StatusService],
   exports: [StatusService, TypeOrmModule.forFeature([Status])],
 })
 export class StatusModule {}
