0d8ceb7c2ac8a477945847068bff969c99f34b91
martin <martin.aubertin@90tech.fr>
2024-04-05 09:31:03 +0200

    changes

--- frontend/src/components/atoms/TextBlock/TextBlock.tsx
+++ frontend/src/components/atoms/TextBlock/TextBlock.tsx
@@ -3,36 +3,37 @@ import React, { useState } from 'react';
 import './TextBlock.css';
 
 interface TextBlockProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    variant: TextFieldVariants;
-    rows: number;
-    maxChars: number;
+  variant: TextFieldVariants;
+  rows: number;
+  maxChars: number;
 }
 
 export const TextBlock: React.FC<TextBlockProps> = (props: TextBlockProps) => {
-    const { variant, rows, maxChars } = props;
-    const [ charCount, setCharCount ] = useState<number>(0);
-    const [ isError, setIsError ] = useState<boolean>(false);
+  const { variant, rows, maxChars } = props;
+  const [charCount, setCharCount] = useState<number>(0);
+  const [isError, setIsError] = useState<boolean>(false);
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const charText = event.target.value;
-        setCharCount(charText.length);
-        setIsError(charText.length >= maxChars)
-    };
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const charText = event.target.value;
+    setCharCount(charText.length);
+    setIsError(charText.length >= maxChars)
+  };
 
   return (
     <TextField
-        type={'text'}
-        variant={variant}
-        multiline
-        rows={rows}
-        fullWidth
-        inputProps={{ maxLength: maxChars }}
-        onChange={handleChange}
-        InputProps={{ endAdornment:
-          <InputAdornment position='end' className={isError ? 'red-text' : ''} sx={{mt: 20}}>
+      type={'text'}
+      variant={variant}
+      multiline
+      rows={rows}
+      fullWidth
+      inputProps={{ maxLength: maxChars }}
+      onChange={handleChange}
+      InputProps={{
+        endAdornment:
+          <InputAdornment position='end' className={isError ? 'red-text' : ''}>
             {charCount}/{maxChars}
           </InputAdornment>
-        }}
+      }}
     />
   );
 };
