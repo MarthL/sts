f6d74c13de2a6a575ce306c12f62b1d9fc9374a2
Martin <martin.aubertin@gmail.com>
2024-06-27 12:48:42 +0700

    review fe collab list

--- frontend/src/components/molecules/UserListCustom/UserListCustom.tsx
+++ frontend/src/components/molecules/UserListCustom/UserListCustom.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { ListItem, ListItemAvatar, Typography, Avatar, ListItemText } from '@mui/material';
+import { ListItem, Box, ListItemAvatar, Typography, Avatar, ListItemText } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { User, exportProfilePicture } from '../../../api/users';
+
 
 interface UserListCustomProps {
   id: number;
@@ -28,16 +29,18 @@ export const UserListCustom = (props: UserListCustomProps) => {
 
   return (
     <>
-      <ListItem sx={{ width: '100%', borderBottom: 1, marginBlock: '10px' }}>
+      <ListItem sx={{ width: '100%', marginBlock: '10px' }}>
         <Link to={`/users/${id}`}>
-          <ListItemAvatar>
-            <Avatar src={profilePictureUrl} />
-          </ListItemAvatar>
-          <ListItemText>
-            <Typography variant="body1">
-              {username}
-            </Typography>
-          </ListItemText>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <ListItemAvatar>
+              <Avatar src={profilePictureUrl} />
+            </ListItemAvatar>
+            <ListItemText>
+              <Typography variant="body1">
+                {username}
+              </Typography>
+            </ListItemText>
+          </Box>
         </Link>
       </ListItem>
     </>
