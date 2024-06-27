d026705923a5117ce15326217b786319ddcf5fcf
Martin <martin.aubertin@gmail.com>
2024-06-27 12:44:43 +0700

    solve problem with view for companys. todo : remove client duplicate job

--- frontend/src/components/templates/ClientsPage/ClientsPage.tsx
+++ frontend/src/components/templates/ClientsPage/ClientsPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { List, Typography } from '@mui/material';
 import { UserListCustom } from '../../molecules/UserListCustom/UserListCustom';
-import { getAllUsers } from '../../../api/users';
+import { getCompanys } from '../../../api/company';
 // import { User } from '../../../api/users';
 
 export const ClientsPage: React.FC = () => {
@@ -9,14 +9,14 @@ export const ClientsPage: React.FC = () => {
   const [clientsColleciton, setClientsCollection] = useState<any[]>([]);
 
   useEffect(() => {
-    getAllUsers().then(async (res) => {
+    getCompanys().then(async (res) => {
       setClientsCollection(res);
     })
   }, [])
 
   return (
     <>
-      <Typography variant="h3">Collaborators List</Typography>
+      <Typography variant="h3">Company's List</Typography>
       <List sx={{ width: '100%', flexWrap: 'wrap' }}>
         {clientsColleciton.map((client) =>
           client?.name
